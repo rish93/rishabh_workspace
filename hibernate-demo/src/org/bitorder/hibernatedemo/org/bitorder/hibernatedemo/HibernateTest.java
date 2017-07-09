38a531f312157aebc47877a278cf0226e338b79b
ris93 <rish93@gmail.com>
2017-07-09 21:58:12 +0530

    wer

--- hibernate-demo/src/org/bitorder/hibernatedemo/org/bitorder/hibernatedemo/HibernateTest.java
+++ hibernate-demo/src/org/bitorder/hibernatedemo/org/bitorder/hibernatedemo/HibernateTest.java
@@ -19,13 +19,34 @@ eg if we wnat to save something get the session  from session factory and save*/
 	//2 create session from session factory
 	//3 use session to save model object
 		//=========================================
+		//changing value to different it will recreate schema as set to create in config file
+		/*by reading into all model objectss and looking at class name and creating table ouut of it looking at id for properties and creating primary key out of it
+		 *  and will recreate on startuop*/
+		
 		UserDetails user = new UserDetails(); 
-		user.setUserId(4);
-		user.setUsername("fourth user");
+		user.setUserId(1);
+		user.setUsername("first user");
+		//to persist this object use hibernate appi
+		//normally if this was service method we would pass this objdect to dao
+		//dao woul take care to save object using hibernate api
+		/*but here we will use saving object using service method itselff*/
+		
+		
+		
+		/*USING HIBERNATE API  
+		 * create session factory (session factory is only one object per application ) responsibility of this session is to
+		 * create session depending on how many other sessions you want in your application throughout execution period
+		 *-> session factory need to be created so that session object can be made which can further used in saving...by this sessu=ion factory get hold of session object 
+		 * */
+		
+		
+		
+		
 	//tp get the session factory we need to get configuration
 	//open up already made configuration file by calling Configuration()
-	//configure() reads up the xml file	
+	//configure() reads up the xml file	//configure takes configuration file reads it for database configuration
     //call build session factoy method that return session factory
+		//sessionFactory can be used to create new sessions
 		//=========================================
 		SessionFactory sessionFactory =new Configuration().configure().buildSessionFactory();
 	
@@ -38,7 +59,7 @@ eg if we wnat to save something get the session  from session factory and save*/
 	
 	//before saving we need to open transaction
 	//(in a single transaction we can save as many model object)
-	//its a unit of work
+	//its a single unit of work
 	//=========================================
 	session.beginTransaction();
 	session.save(user);
@@ -46,6 +67,20 @@ eg if we wnat to save something get the session  from session factory and save*/
 	//to end the transaction first get the transaction then commit it
 	
 	//(for error handling put everything in try block and in catch put rollback)
+	
+	//===========================================================================================================================
+	//===========================================================================================================================
+
+	//hibernate is an ORM tool 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 	
 }
--- hibernate-demo/src/org/bitorder/hibernatedemo/org/bitorder/hibernatedemo/dto/UserDetails.java
+++ hibernate-demo/src/org/bitorder/hibernatedemo/org/bitorder/hibernatedemo/dto/UserDetails.java
@@ -4,8 +4,35 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-//model cLASS
-@Entity  //name attribute will not make table from clss name instead will consider attribute name specified
+//model cLASS dto//can have as many model class by using Entity and id it amodel class
+/*have another class taht instantiate model class and will pass it to hibernate to save it
+ * here main class */
+
+
+@Entity  
+/*An Entity is roughly the same thing as an instance of a class
+ *  when you are thinking from a code perspective or a row in a
+ *   table (basically) when you are thinking from a database
+ *    perspective.
+So, it's essentially a persisted / persistable instance
+ of a class. Changing values on it works just like
+  changing values on any other class instance.
+   The difference is that you can persist those 
+   changes and, in general, the current state of the class
+    instance (entity) will overwrite the values the row
+     for that instance (entity) had in the database, based on
+      the primary key in the database matching the "id" or 
+      similar
+ field in the class instance (entity).*/
+
+
+//name attribute will not make table from clss 
+//name instead will consider attribute name specified
+//treat this class as entity need to save this , in xml name class is also give in then y need here coz there are other ways these object can be annotatesd without having to be entity
+//hibernate look at those classes where entity or annotation belongs
+
+
+//now have a class that nstantiate these objects
 
 @Table(name="userdetails")
 public class UserDetails {
@@ -15,7 +42,7 @@ public class UserDetails {
 	private int userId;
 
 	private String username;
-	@Id
+	@Id        //to make primary key//treat value of this field as primary key
 	public int getUserId() {
 		return userId;
 	}
